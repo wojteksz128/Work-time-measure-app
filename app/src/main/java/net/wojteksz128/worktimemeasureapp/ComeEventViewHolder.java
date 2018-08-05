c79bf5a450b43d677ab43ed942ad0403b32b1e5c
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-08-06 00:06:01 +0200

    Implement view holder

--- app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
@@ -2,10 +2,23 @@ package net.wojteksz128.worktimemeasureapp;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import net.wojteksz128.worktimemeasureapp.database.ComeEvent;
 
 public class ComeEventViewHolder extends RecyclerView.ViewHolder {
 
-    public ComeEventViewHolder(View itemView) {
+    final TextView dateTV;
+    final TextView typeTV;
+
+
+    public ComeEventViewHolder(View itemView, ComeEvent comeEvent) {
         super(itemView);
+
+        dateTV = itemView.findViewById(R.id.day_item_date);
+        typeTV = itemView.findViewById(R.id.day_item_type);
+
+        dateTV.setText(comeEvent.getDate().toString());
+        typeTV.setText(comeEvent.getType().name());
     }
 }
