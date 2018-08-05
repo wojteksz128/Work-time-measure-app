3f98c5cba378221044bb5a290b95a41500da06f9
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-08-05 15:23:34 +0200

    Add FAB into layout and show information after click

--- app/src/main/java/net/wojteksz128/worktimemeasureapp/MainActivity.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/MainActivity.java
@@ -1,7 +1,11 @@
 package net.wojteksz128.worktimemeasureapp;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout layout = findViewById(R.id.main_layout);
+        final FloatingActionButton enterFab = findViewById(R.id.enterFab);
+        enterFab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(layout, "Dodano nową pozycję.", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
