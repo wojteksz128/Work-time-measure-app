8882619a062689cc650a59151f71d90ec825a481
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-08-05 23:51:56 +0200

    Create Stub ViewHolder and Adapter

---
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventAdapter.java
@@ -0,0 +1,24 @@
+package net.wojteksz128.worktimemeasureapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ComeEventAdapter extends RecyclerView.Adapter<ComeEventViewHolder> {
+
+    @NonNull
+    @Override
+    public ComeEventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ComeEventViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
@@ -0,0 +1,11 @@
+package net.wojteksz128.worktimemeasureapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ComeEventViewHolder extends RecyclerView.ViewHolder {
+
+    public ComeEventViewHolder(View itemView) {
+        super(itemView);
+    }
+}
