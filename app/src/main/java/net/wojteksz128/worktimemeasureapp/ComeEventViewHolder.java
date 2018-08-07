3c4696a4c8939108c35a16faa29c1516ffb54cc3
Wojciech Szczepaniak <wojciech.szczepaniak@comarch.pl>
2018-08-07 13:45:31 +0200

    Add background into come type view

--- app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
@@ -10,17 +10,23 @@ public class ComeEventViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView dateTV;
     private final TextView typeTV;
+    private final View view;
 
 
     public ComeEventViewHolder(View itemView) {
         super(itemView);
 
+        view = itemView;
         dateTV = itemView.findViewById(R.id.day_item_date);
         typeTV = itemView.findViewById(R.id.day_item_type);
     }
 
     public void bind(ComeEvent comeEvent) {
         dateTV.setText(comeEvent.getDate().toString());
+
         typeTV.setText(comeEvent.getType().name());
+        // TODO: 2018-08-07 Solve problem with API versions
+        typeTV.setBackground(view.getContext().getDrawable(comeEvent.getType().getBackground()));
+        typeTV.setPadding(4, 4, 4, 4);
     }
 }
--- app/src/main/java/net/wojteksz128/worktimemeasureapp/database/ComeEventType.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/database/ComeEventType.java
@@ -1,6 +1,18 @@
 package net.wojteksz128.worktimemeasureapp.database;
 
+import net.wojteksz128.worktimemeasureapp.R;
+
 public enum ComeEventType {
-    COME_IN,
-    COME_OUT
+    COME_IN(R.drawable.come_in_background),
+    COME_OUT(R.drawable.come_out_background);
+
+    private final int background;
+
+    ComeEventType(int background) {
+        this.background = background;
+    }
+
+    public int getBackground() {
+        return background;
+    }
 }
