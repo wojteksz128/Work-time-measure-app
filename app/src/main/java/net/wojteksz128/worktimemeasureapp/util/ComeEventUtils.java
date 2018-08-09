15222071435666ec0c0e54a26e96745dec26facf
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-08-09 23:38:18 +0200

    Repair time count

--- app/src/main/java/net/wojteksz128/worktimemeasureapp/util/ComeEventUtils.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/util/ComeEventUtils.java
@@ -15,9 +15,7 @@ import net.wojteksz128.worktimemeasureapp.database.workDay.WorkDayEvents;
 import java.util.ArrayList;
 import java.util.Date;
 
-// TODO: 09.08.2018 Zaimplementuj pooprawne obliczenia czasu (przechowuj i licz w UTC - przeliczaj na bieżący)
 public class ComeEventUtils {
-
 
     public static void registerNewEvent(Context context, final Function<Void, Void> preFunction, final Function<ComeEventType, Void> postFunction) {
         final ComeEventDao comeEventDao = AppDatabase.getInstance(context).comeEventDao();
