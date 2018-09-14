514ee06cf06ad4cc057ffa268589552f58b9e8e1
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-09-14 19:37:52 +0200

    Extract fill TextViews in ComeEventViewHolder

--- app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
+++ app/src/main/java/net/wojteksz128/worktimemeasureapp/ComeEventViewHolder.java
@@ -9,38 +9,49 @@ import net.wojteksz128.worktimemeasureapp.util.DateTimeUtils;
 
 import java.util.TimeZone;
 
-// DONE: 10.08.2018 Change view and viewholder implementations
+// DONE: 10.08.2018 Change mView and viewholder implementations
 public class ComeEventViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView mStartDateTV;
     private final TextView mEndDateTV;
     private final TextView mDurationTV;
 
-    private final View view;
+    private final View mView;
 
 
     @SuppressWarnings("WeakerAccess")
     public ComeEventViewHolder(View itemView) {
         super(itemView);
 
-        view = itemView;
+        mView = itemView;
         mStartDateTV = itemView.findViewById(R.id.main_day_event_start_date);
         mEndDateTV = itemView.findViewById(R.id.main_day_event_end_date);
         mDurationTV = itemView.findViewById(R.id.main_day_event_duration);
     }
 
     public void bind(ComeEvent comeEvent) {
-        final String startTime = DateTimeUtils.formatDate(view.getContext().getString(R.string.main_day_event_time_format), comeEvent.getStartDate());
-        final String endTime = comeEvent.getEndDate() != null ? DateTimeUtils.formatDate(view.getContext().getString(R.string.main_day_event_time_format), comeEvent.getEndDate()) : "Teraz";
-        final String duration = comeEvent.getDuration() != null ? DateTimeUtils.formatDate(view.getContext().getString(R.string.main_work_day_duration_format),
-                comeEvent.getDuration(), TimeZone.getTimeZone("UTC")) : "";
+        fillStartTime(comeEvent);
+        fillEndTime(comeEvent);
+        fillDuration(comeEvent);
+    }
 
-        mStartDateTV.setText(startTime);
-        mEndDateTV.setText(endTime);
+    private void fillDuration(ComeEvent comeEvent) {
+        final String duration = comeEvent.getDuration() != null ? DateTimeUtils.formatDate(mView.getContext().getString(R.string.main_work_day_duration_format),
+                comeEvent.getDuration(), TimeZone.getTimeZone("UTC")) : "";
         mDurationTV.setText(duration);
     }
 
+    private void fillEndTime(ComeEvent comeEvent) {
+        final String endTime = comeEvent.getEndDate() != null ? DateTimeUtils.formatDate(mView.getContext().getString(R.string.main_day_event_time_format), comeEvent.getEndDate()) : "Teraz";
+        mEndDateTV.setText(endTime);
+    }
+
+    private void fillStartTime(ComeEvent comeEvent) {
+        final String startTime = DateTimeUtils.formatDate(mView.getContext().getString(R.string.main_day_event_time_format), comeEvent.getStartDate());
+        mStartDateTV.setText(startTime);
+    }
+
     public View getView() {
-        return view;
+        return mView;
     }
 }
