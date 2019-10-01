1d4ca152c505606b7da86a16b0ad5a91b7f572b9
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2019-10-01 20:24:41 +0200

    Set other workTimeDuration

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/configuration/ConfigurationProvider.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/configuration/ConfigurationProvider.kt
@@ -3,7 +3,8 @@ package net.wojteksz128.worktimemeasureapp.configuration
 import org.joda.time.Duration
 
 object ConfigurationProvider {
+    // TODO wszczepaniak: Dodaj okno konfiguracji.
     val workTimeDuration: Duration
-        //        get() = Duration.millis(8*60*60*1000 + 30*60*1000)
-        get() = Duration.millis(10 * 1000)
+        get() = Duration.millis(8 * 60 * 60 * 1000 + 30 * 60 * 1000)
+//        get() = Duration.millis(10 * 1000)
 }
