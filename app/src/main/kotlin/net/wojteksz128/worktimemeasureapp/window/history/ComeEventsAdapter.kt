7c700eee53cc2d9b2bc4b16a24d789cb3d571752
Wojciech Szczepaniak <wojciech.szczepaniak@comarch.pl>
2021-09-09 13:42:49 +0200

    Apply data binding into WorkDay recycler view

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/ComeEventsAdapter.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/ComeEventsAdapter.kt
@@ -9,7 +9,7 @@ import net.wojteksz128.worktimemeasureapp.database.comeEvent.ComeEvent
 import net.wojteksz128.worktimemeasureapp.databinding.HistoryDayEventListItemBinding
 
 class ComeEventsAdapter :
-    ListAdapter<ComeEvent, ComeEventsAdapter.ComeEventViewHolder>(ComeEventDiffCallback) {
+    ListAdapter<ComeEvent, ComeEventsAdapter.ComeEventViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComeEventViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -30,16 +30,16 @@ class ComeEventsAdapter :
             binding.comeEvent = comeEvent
         }
     }
-}
 
 
-object ComeEventDiffCallback : DiffUtil.ItemCallback<ComeEvent>() {
-    override fun areItemsTheSame(oldItem: ComeEvent, newItem: ComeEvent): Boolean {
-        return oldItem == newItem
+    object DiffCallback : DiffUtil.ItemCallback<ComeEvent>() {
+        override fun areItemsTheSame(oldItem: ComeEvent, newItem: ComeEvent): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: ComeEvent, newItem: ComeEvent): Boolean {
+            return oldItem.id == newItem.id
+        }
+
     }
-
-    override fun areContentsTheSame(oldItem: ComeEvent, newItem: ComeEvent): Boolean {
-        return oldItem.id == newItem.id
-    }
-
 }
--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/WorkDayAdapter.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/WorkDayAdapter.kt
@@ -1,23 +1,26 @@
 package net.wojteksz128.worktimemeasureapp.window.history
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import net.wojteksz128.worktimemeasureapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.wojteksz128.worktimemeasureapp.WorkTimeMeasureApp.Companion.context
 import net.wojteksz128.worktimemeasureapp.database.workDay.WorkDayEvents
+import net.wojteksz128.worktimemeasureapp.databinding.HistoryWorkDayListItemBinding
+import net.wojteksz128.worktimemeasureapp.util.ClassTagAware
 import net.wojteksz128.worktimemeasureapp.util.FunctionWithParameter
+import net.wojteksz128.worktimemeasureapp.util.PeriodicOperationRunner
 
-internal class WorkDayAdapter(private val uiThreadRunner: (Runnable) -> Unit) :
-    PagingDataAdapter<WorkDayEvents, WorkDayViewHolder>(DiffCallback()) {
+class WorkDayAdapter(private val uiThreadRunner: (Runnable) -> Unit) :
+    PagingDataAdapter<WorkDayEvents, WorkDayAdapter.WorkDayViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkDayViewHolder {
-
-        val context = parent.context
         val inflater = LayoutInflater.from(context)
-
-        val view = inflater.inflate(R.layout.history_work_day_list_item, parent, false)
-        return WorkDayViewHolder(view)
+        val binding = HistoryWorkDayListItemBinding.inflate(inflater, parent, false)
+        return WorkDayViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: WorkDayViewHolder, position: Int) {
@@ -38,7 +41,47 @@ internal class WorkDayAdapter(private val uiThreadRunner: (Runnable) -> Unit) :
     }
 
 
-    private class DiffCallback : DiffUtil.ItemCallback<WorkDayEvents>() {
+    class WorkDayViewHolder(private val binding: HistoryWorkDayListItemBinding) :
+        RecyclerView.ViewHolder(binding.root), ClassTagAware {
+
+        private val secondRunner = PeriodicOperationRunner<WorkDayEvents>()
+        private val comeEventsAdapter = ComeEventsAdapter()
+
+        init {
+            binding.dayEventsList.adapter = comeEventsAdapter
+            binding.dayEventsList.layoutManager = object : LinearLayoutManager(context) {
+                override fun canScrollVertically() = false
+            }
+        }
+
+        fun bind(workDay: WorkDayEvents, updateAction: FunctionWithParameter<WorkDayEvents>) {
+            binding.workDay = workDay
+
+            comeEventsAdapter.submitList(workDay.events)
+            prepareCountingIfAnyEventNotFinished(workDay, updateAction)
+        }
+
+        private fun prepareCountingIfAnyEventNotFinished(
+            workDay: WorkDayEvents,
+            updateAction: FunctionWithParameter<WorkDayEvents>
+        ) {
+            if (!workDay.hasEventsEnded()) {
+                if (!secondRunner.isRunning) {
+                    secondRunner.setConsumer(updateAction)
+                    Log.v(
+                        classTag,
+                        "prepareCountingIfAnyEventNotFinished: start second updater for day ${workDay.workDay.date}"
+                    )
+                    secondRunner.start()
+                }
+            } else {
+                secondRunner.stop()
+            }
+        }
+    }
+
+
+    object DiffCallback : DiffUtil.ItemCallback<WorkDayEvents>() {
 
         override fun areItemsTheSame(oldItem: WorkDayEvents, newItem: WorkDayEvents): Boolean {
             return oldItem.workDay.id!! == newItem.workDay.id!!
