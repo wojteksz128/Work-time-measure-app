770400891da8f2dd9974797aabe5b1b9a60de9c7
Wojciech Szczepaniak <wojciech.szczepaniak@comarch.pl>
2021-09-09 14:41:43 +0200

    Apply data binding in history activity with adding hamburger

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/HistoryActivity.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/HistoryActivity.kt
@@ -2,15 +2,14 @@ package net.wojteksz128.worktimemeasureapp.window.history
 
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.viewModels
 import androidx.paging.liveData
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
 import net.wojteksz128.worktimemeasureapp.R
+import net.wojteksz128.worktimemeasureapp.databinding.ActivityHistoryBinding
+import net.wojteksz128.worktimemeasureapp.util.ClassTagAware
 import net.wojteksz128.worktimemeasureapp.util.datetime.DateTimeProvider
+import net.wojteksz128.worktimemeasureapp.window.BaseActivity
 
 // TODO: 09.08.2018 Dodaj joba, który automatycznie zamknie dzień pracy o godzinie zmiany dnia pracy
 // TODO: 11.08.2018 Jeśli aktualny dzień istnieje - przenieś FABa w to miejsce
@@ -22,26 +21,21 @@ import net.wojteksz128.worktimemeasureapp.util.datetime.DateTimeProvider
 // TODO: 11.08.2018 popraw liczenie czasu pracy (może nie brać pod uwagę ms?)
 // TODO: 07.07.2019 Uwzględniaj strefę czasową
 // TODO: 09.06.2021 Uwzględnij przejścia poza jeden dzień oraz możliwość zamknięcia
-class HistoryActivity : AppCompatActivity() {
-    private lateinit var viewModel: HistoryViewModel
-    private lateinit var layout: ConstraintLayout
+class HistoryActivity : BaseActivity<ActivityHistoryBinding>(R.layout.activity_history), ClassTagAware {
+    private val viewModel: HistoryViewModel by viewModels()
+
     private lateinit var workDayAdapter: WorkDayAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
-        Log.v(TAG, "onCreate: Create or get HistoryViewModel object")
-        viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
-
-        layout = findViewById(R.id.history_layout)
 
         initWorkDaysRecyclerView()
     }
 
     override fun onResume() {
         super.onResume()
-        Log.v(TAG, "onResume: Fill days list")
+        Log.v(classTag, "onResume: Fill days list")
         viewModel.workDaysPager.liveData.observe(this,
             { workDayAdapter.submitData(this.lifecycle, it) })
         DateTimeProvider.updateOffset(this)
@@ -49,22 +43,14 @@ class HistoryActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        Log.v(TAG, "onPause: Stop second updater")
+        Log.v(classTag, "onPause: Stop second updater")
         this.viewModel.secondRunner.stop()
     }
 
     private fun initWorkDaysRecyclerView() {
         workDayAdapter = WorkDayAdapter { action: Runnable -> runOnUiThread(action) }
 
-        val layoutManager = LinearLayoutManager(this)
-
-        val mDayList = findViewById<RecyclerView>(R.id.history_rv_days)
-        mDayList.layoutManager = layoutManager
-        mDayList.adapter = workDayAdapter
-        (mDayList.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
-    }
-
-    companion object {
-        private val TAG = HistoryActivity::class.java.simpleName
+        binding.historyRvDays.adapter = workDayAdapter
+        (binding.historyRvDays.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
     }
 }
