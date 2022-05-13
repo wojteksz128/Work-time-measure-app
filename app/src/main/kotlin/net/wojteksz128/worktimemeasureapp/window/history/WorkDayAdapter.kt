d631a33076147f491f811fbbbcaa8c82f8cda2a5
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-05-13 09:57:04 +0200

    Make in history fragment comeEvent elements editable

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/WorkDayAdapter.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/history/WorkDayAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LifecycleOwner
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -11,26 +12,43 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import net.wojteksz128.worktimemeasureapp.databinding.ListItemHistoryWorkDayBinding
+import net.wojteksz128.worktimemeasureapp.model.ComeEvent
 import net.wojteksz128.worktimemeasureapp.model.WorkDay
 import net.wojteksz128.worktimemeasureapp.util.ClassTagAware
 import net.wojteksz128.worktimemeasureapp.util.datetime.DateTimeUtils
 import net.wojteksz128.worktimemeasureapp.util.livedata.RecyclerViewPeriodicUpdater
 import net.wojteksz128.worktimemeasureapp.util.recyclerView.RecyclerViewItemClick
+import net.wojteksz128.worktimemeasureapp.util.recyclerView.ViewHolderInformation
+import net.wojteksz128.worktimemeasureapp.window.history.ComeEventsAdapter.ComeEventViewHolder
 import net.wojteksz128.worktimemeasureapp.window.util.button.ExpandViewModel
+import net.wojteksz128.worktimemeasureapp.window.util.recyclerView.ComeEventsRecyclerViewSwipeLogic
 
 class WorkDayAdapter(
     private val context: Context,
     private val dateTimeUtils: DateTimeUtils,
+    private val fragmentManager: FragmentManager,
     private val lifecycleOwner: LifecycleOwner,
-    override var onItemClickListenerProvider: (WorkDay) -> (View) -> Unit = { {} },
+    private val workDayItemListener: WorkDayItemListener
 ) : PagingDataAdapter<WorkDay, WorkDayAdapter.WorkDayViewHolder>(WorkDayEventsDiffCallback),
     RecyclerViewItemClick<WorkDay> {
     private val periodicUpdater = RecyclerViewPeriodicUpdater(this)
 
+    @Suppress("UNUSED_PARAMETER")
+    override var onItemClickListenerProvider: (WorkDay) -> (View) -> Unit
+        get() = workDayItemListener::onWorkDayClicked
+        set(value) {}
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkDayViewHolder {
         val inflater = LayoutInflater.from(context)
         val binding = ListItemHistoryWorkDayBinding.inflate(inflater, parent, false)
-        return WorkDayViewHolder(binding, context, lifecycleOwner, dateTimeUtils)
+        return WorkDayViewHolder(
+            binding,
+            context,
+            fragmentManager,
+            lifecycleOwner,
+            dateTimeUtils,
+            workDayItemListener::onWorkDayEventSelected
+        )
     }
 
     override fun onBindViewHolder(holder: WorkDayViewHolder, position: Int) {
@@ -57,30 +75,39 @@ class WorkDayAdapter(
     class WorkDayViewHolder(
         val binding: ListItemHistoryWorkDayBinding,
         context: Context,
-        lifecycleOwner: LifecycleOwner,
-        dateTimeUtils: DateTimeUtils,
+        private val fragmentManager: FragmentManager,
+        private val lifecycleOwner: LifecycleOwner,
+        private val dateTimeUtils: DateTimeUtils,
+        private val selectionUpdater: (ComeEvent, ViewHolderInformation<ComeEventViewHolder>) -> Unit,
     ) : RecyclerView.ViewHolder(binding.root), ClassTagAware {
         private val expandViewModel = ExpandViewModel()
 
         private val comeEventsAdapter = ComeEventsAdapter(dateTimeUtils)
 
         init {
-            binding.lifecycleOwner = lifecycleOwner
-            binding.dateTimeUtils = dateTimeUtils
-            binding.dayEventsList.apply {
-
-                adapter = comeEventsAdapter
-                layoutManager = object : LinearLayoutManager(context) {
-                    override fun canScrollVertically() = false
+            binding.apply {
+                lifecycleOwner = this@WorkDayViewHolder.lifecycleOwner
+                dateTimeUtils = this@WorkDayViewHolder.dateTimeUtils
+                dayEventsList.apply {
+                    adapter = comeEventsAdapter
+                    layoutManager = object : LinearLayoutManager(context) {
+                        override fun canScrollVertically() = false
+                    }
+                    addItemDecoration(
+                        DividerItemDecoration(
+                            context,
+                            DividerItemDecoration.VERTICAL
+                        )
+                    )
                 }
-                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                ComeEventsRecyclerViewSwipeLogic(context, selectionUpdater)
+                    .attach(dayEventsList, fragmentManager)
+                expandViewModel = this@WorkDayViewHolder.expandViewModel
             }
-            binding.expandViewModel = expandViewModel
         }
 
         fun bind(workDay: WorkDay) {
             binding.workDay = workDay
-
 
             comeEventsAdapter.submitList(workDay.events)
         }
@@ -90,9 +117,7 @@ class WorkDayAdapter(
         }
 
         fun setOnClickListener(onItemClickListener: (View) -> Unit) {
-            itemView.setOnClickListener(onItemClickListener)
-            binding.dayEventsList.setOnClickListener(onItemClickListener)
-            comeEventsAdapter.onItemClickListenerProvider = { onItemClickListener }
+            binding.dayLabelContainer.setOnClickListener(onItemClickListener)
         }
     }
 
@@ -105,6 +130,15 @@ class WorkDayAdapter(
 
         override fun areContentsTheSame(oldItem: WorkDay, newItem: WorkDay) =
             oldItem == newItem
+    }
+
+    interface WorkDayItemListener {
+        fun onWorkDayEventSelected(
+            comeEvent: ComeEvent,
+            viewHolderInformation: ViewHolderInformation<ComeEventViewHolder>
+        )
+
+        fun onWorkDayClicked(workDay: WorkDay): (View) -> Unit = {}
     }
 }
 
