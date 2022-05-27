d599ee5bb53c743357656c14a9269c9f87d39d82
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-05-27 14:39:05 +0200

    Add settings of days off public

---
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/window/settings/DaysOffFragment.kt
@@ -0,0 +1,7 @@
+package net.wojteksz128.worktimemeasureapp.window.settings
+
+import dagger.hilt.android.AndroidEntryPoint
+import net.wojteksz128.worktimemeasureapp.R
+
+@AndroidEntryPoint
+class DaysOffFragment : BasePreferenceFragment(R.xml.days_off_preferences)
