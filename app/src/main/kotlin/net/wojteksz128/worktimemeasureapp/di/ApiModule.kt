c0c380768153a9c41304f802eaaf75f2ab491c08
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-05-31 09:52:48 +0200

    Add NagerDateApiV3Service instance creation using Hilt

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/di/ApiModule.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/di/ApiModule.kt
@@ -7,16 +7,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import net.wojteksz128.worktimemeasureapp.api.holidayapi.HolidayApiService
+import net.wojteksz128.worktimemeasureapp.api.nagerDate.NagerDateApiV3Service
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
     private const val HOLIDAY_API_BASE_URL = "https://holidayapi.com/v1/"
+    private const val NAGER_DATE_API_BASE_URL = "https://date.nager.at/api/"
 
     @Singleton
     @Provides
@@ -42,7 +45,8 @@ object ApiModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit =
+    @Named("holiday_api_retrofit")
+    fun provideHolidayApiRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit =
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create(gson))
             .baseUrl(HOLIDAY_API_BASE_URL)
@@ -51,6 +55,21 @@ object ApiModule {
 
     @Singleton
     @Provides
-    fun provideHolidayApiService(retrofit: Retrofit): HolidayApiService =
+    fun provideHolidayApiService(@Named("holiday_api_retrofit") retrofit: Retrofit): HolidayApiService =
         retrofit.create(HolidayApiService::class.java)
+
+    @Singleton
+    @Provides
+    @Named("nager_date_api_retrofit")
+    fun provideNagerDateApiRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(NAGER_DATE_API_BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideNagerDateApiV3Service(@Named("nager_date_api_retrofit") retrofit: Retrofit): NagerDateApiV3Service =
+        retrofit.create(NagerDateApiV3Service::class.java)
 }
