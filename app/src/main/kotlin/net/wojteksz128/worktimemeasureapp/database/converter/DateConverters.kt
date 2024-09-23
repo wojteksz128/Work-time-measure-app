be67448517064533ffc001a3d6069ec85ed6e5f2
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2024-09-23 14:43:31 +0200

    BUGFIX Store and load date time using UTC timezone

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/converter/DateConverters.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/converter/DateConverters.kt
@@ -11,17 +11,20 @@ import java.util.Date
 class DateConverters {
 
     private val localDateFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+    private val storeTimeZone = ZoneId.of("UTC")
 
     @TypeConverter
     fun toDate(localDateTimeString: String?): Date? = localDateTimeString?.let {
         val localDateTime = LocalDateTime.parse(localDateTimeString, localDateFormatter)
-        val instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant()
+        val zonedDateTime = localDateTime.atZone(storeTimeZone)
+        val instant = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toInstant()
         Date(instant.toEpochMilli())
     }
 
     @TypeConverter
     fun toTimestamp(date: Date?): String? = date?.let {
-        Instant.ofEpochMilli(it.time).atZone(ZoneId.systemDefault()).format(localDateFormatter)
+        Instant.ofEpochMilli(it.time).atZone(ZoneId.systemDefault())
+            .withZoneSameInstant(storeTimeZone).format(localDateFormatter)
     }
 
     @TypeConverter
