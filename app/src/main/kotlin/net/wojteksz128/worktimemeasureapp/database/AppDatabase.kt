25da69eda559f1b8e2d06dd4a59bfd1c58061cfa
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2024-09-23 14:20:06 +0200

    Convert date timestamp to ISO-8601 string format

--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/AppDatabase.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/AppDatabase.kt
@@ -9,7 +9,16 @@ import net.wojteksz128.worktimemeasureapp.database.comeEvent.ComeEventDto
 import net.wojteksz128.worktimemeasureapp.database.converter.DateConverters
 import net.wojteksz128.worktimemeasureapp.database.dayOff.DayOffDao
 import net.wojteksz128.worktimemeasureapp.database.dayOff.DayOffDto
-import net.wojteksz128.worktimemeasureapp.database.migration.*
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom10To11
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom1To2
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom2To3
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom3To4
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom4To5
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom5To6
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom6To7
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom7To8
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom8To9
+import net.wojteksz128.worktimemeasureapp.database.migration.MigrateFrom9To10
 import net.wojteksz128.worktimemeasureapp.database.workDay.WorkDayDao
 import net.wojteksz128.worktimemeasureapp.database.workDay.WorkDayDto
 import net.wojteksz128.worktimemeasureapp.util.ClassTagAware
@@ -20,7 +29,7 @@ import net.wojteksz128.worktimemeasureapp.util.ClassTagAware
         WorkDayDto::class,
         DayOffDto::class
     ],
-    version = 10
+    version = 11
 )
 @TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
@@ -45,6 +54,7 @@ abstract class AppDatabase : RoomDatabase() {
                 MigrateFrom7To8(),
                 MigrateFrom8To9(),
                 MigrateFrom9To10(),
+                MigrateFrom10To11()
             )
     }
 }
--- app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/converter/DateConverters.kt
+++ app/src/main/kotlin/net/wojteksz128/worktimemeasureapp/database/converter/DateConverters.kt
@@ -1,28 +1,34 @@
 package net.wojteksz128.worktimemeasureapp.database.converter
 
 import androidx.room.TypeConverter
+import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneId
+import org.threeten.bp.format.DateTimeFormatter
 import java.util.Date
 
 class DateConverters {
 
+    private val localDateFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
     @TypeConverter
-    fun toDate(timestamp: Long?): Date? {
-        return if (timestamp == null) null else Date(timestamp)
+    fun toDate(localDateTimeString: String?): Date? = localDateTimeString?.let {
+        val localDateTime = LocalDateTime.parse(localDateTimeString, localDateFormatter)
+        val instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant()
+        Date(instant.toEpochMilli())
     }
 
     @TypeConverter
-    fun toTimestamp(date: Date?): Long? {
-        return date?.time
+    fun toTimestamp(date: Date?): String? = date?.let {
+        Instant.ofEpochMilli(it.time).atZone(ZoneId.systemDefault()).format(localDateFormatter)
     }
 
     @TypeConverter
-    fun toLocalDate(dateString: String?): LocalDate? =
-        dateString?.let {
-            LocalDate.parse(dateString)
-        }
+    fun toLocalDate(dateString: String?): LocalDate? = dateString?.let {
+        LocalDate.parse(dateString)
+    }
 
     @TypeConverter
-    fun toLocalDateString(date: LocalDate?): String? =
-        date?.toString()
+    fun toLocalDateString(date: LocalDate?): String? = date?.toString()
 }
