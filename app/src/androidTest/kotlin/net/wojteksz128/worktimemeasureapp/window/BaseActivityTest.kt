673b92ce6f58078e4753037f9a4916fb670ca514
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2025-10-24 10:24:50 +0200

    Tests for ProfileFragment and effects of changing are created

--- app/src/androidTest/kotlin/net/wojteksz128/worktimemeasureapp/window/BaseActivityTest.kt
+++ app/src/androidTest/kotlin/net/wojteksz128/worktimemeasureapp/window/BaseActivityTest.kt
@@ -1,14 +1,25 @@
 package net.wojteksz128.worktimemeasureapp.window
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Context
+import android.content.Intent
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -20,7 +31,10 @@ import net.wojteksz128.worktimemeasureapp.R
 import net.wojteksz128.worktimemeasureapp.model.fieldType.DayType
 import net.wojteksz128.worktimemeasureapp.module.dayOff.DayOffService
 import net.wojteksz128.worktimemeasureapp.settings.InitialSettingsPreparer
+import net.wojteksz128.worktimemeasureapp.util.createTestImageUri
+import net.wojteksz128.worktimemeasureapp.util.isDefaultImage
 import net.wojteksz128.worktimemeasureapp.window.dashboard.DashboardActivity
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -53,6 +67,7 @@ class BaseActivityTest {
 
     @Before
     fun setup() {
+        Intents.init()
         hiltRule.inject()
 
         initialSettingsPreparer.initSettings()
@@ -67,6 +82,7 @@ class BaseActivityTest {
 
     @After
     fun tearDown() {
+        Intents.release()
         scenario.close()
     }
 
@@ -110,6 +126,60 @@ class BaseActivityTest {
     fun test_aboutSnackbar() {
         navigateTo(R.id.nav_about)
         onView(withText(R.string.about)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenProfileDataChanged_thenDrawerHeaderIsUpdated() {
+        val newUsername = "John Doe"
+        val newEmail = "john.doe@example.com"
+
+        // Navigate to Settings
+        navigateTo(R.id.nav_settings)
+
+        // Navigate to ProfileFragment
+        onView(withText(R.string.settings_header_profile_title)).perform(click())
+
+        // Change username
+        onView(withText(R.string.settings_profile_username_title)).perform(click())
+        onView(withId(android.R.id.edit)).perform(replaceText(newUsername))
+        onView(withText("OK")).perform(click())
+
+        // Change email
+        onView(withText(R.string.settings_profile_email_title)).perform(click())
+        onView(withId(android.R.id.edit)).perform(replaceText(newEmail))
+        onView(withText("OK")).perform(click())
+
+        // Go back to DashboardActivity
+        pressBack() // Back from ProfileFragment to SettingsActivity
+        pressBack() // Back from SettingsActivity to DashboardActivity
+
+        // Open Drawer and check if header is updated
+        onView(withId(R.id.base_drawer_layout)).perform(DrawerActions.open())
+        onView(withText(newUsername)).check(matches(isDisplayed()))
+        onView(withText(newEmail)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenProfileImageChanged_thenDrawerHeaderIsUpdated() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val testImageUri = createTestImageUri(context)
+
+        val resultData = Intent().setData(testImageUri)
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+        intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(result)
+
+        navigateTo(R.id.nav_settings)
+        onView(withText(R.string.settings_header_profile_title)).perform(click())
+
+        onView(withText(R.string.image_view_preference_image_hint)).perform(click())
+        intended(hasAction(Intent.ACTION_GET_CONTENT))
+
+        pressBack()
+        pressBack()
+
+        onView(withId(R.id.base_drawer_layout)).perform(DrawerActions.open())
+        onView(withId(R.id.base_navbar_header_profile_image))
+            .check(matches(not(isDefaultImage(context))))
     }
 
     private fun navigateTo(navId: Int) {
--- app/src/androidTest/kotlin/net/wojteksz128/worktimemeasureapp/window/settings/ProfileFragmentTest.kt
+++ app/src/androidTest/kotlin/net/wojteksz128/worktimemeasureapp/window/settings/ProfileFragmentTest.kt
@@ -1,17 +1,40 @@
 package net.wojteksz128.worktimemeasureapp.window.settings
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Context
+import android.content.Intent
+import androidx.compose.ui.test.isEnabled
+import androidx.preference.PreferenceManager
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import net.wojteksz128.worktimemeasureapp.R
+import net.wojteksz128.worktimemeasureapp.settings.InitialSettingsPreparer
+import net.wojteksz128.worktimemeasureapp.util.createTestImageUri
+import net.wojteksz128.worktimemeasureapp.util.getInternalImageUri
 import net.wojteksz128.worktimemeasureapp.util.launchFragmentInHiltContainer
+import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
@@ -20,11 +43,121 @@ class ProfileFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @Inject
+    lateinit var initialSettingsPreparer: InitialSettingsPreparer
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        Intents.init()
+        hiltRule.inject()
+        context = ApplicationProvider.getApplicationContext()
+        initialSettingsPreparer.initSettings()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
     @Test
     fun givenFragmentStarted_thenDisplaysProfilePreferences() {
         launchFragmentInHiltContainer<ProfileFragment>()
 
+        onView(withText(R.string.image_view_preference_image_hint)).check(matches(isDisplayed()))
         onView(withText(R.string.settings_profile_username_title)).check(matches(isDisplayed()))
         onView(withText(R.string.settings_profile_email_title)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenProfileImageChanged_thenValueIsSaved() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val testImageUri = createTestImageUri(context)
+
+        val resultData = Intent().setData(testImageUri)
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+        intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(result)
+
+        launchFragmentInHiltContainer<ProfileFragment>()
+
+        onView(withText(R.string.image_view_preference_image_hint)).perform(click())
+        intended(hasAction(Intent.ACTION_GET_CONTENT))
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        assertEquals(
+            getInternalImageUri(context, testImageUri),
+            sharedPreferences.getString(context.getString(R.string.settings_key_profile_image), "")
+        )
+    }
+
+    @Test
+    fun whenUsernameChanged_thenValueIsSaved() {
+        val newUsername = "John Doe"
+        launchFragmentInHiltContainer<ProfileFragment>()
+
+        // Click on username preference
+        onView(withText(R.string.settings_profile_username_title)).perform(click())
+
+        // Type new username and click OK
+        onView(withId(android.R.id.edit)).perform(replaceText(newUsername))
+        onView(withText("OK")).perform(click())
+
+        // Verify that the preference was saved
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        assertEquals(
+            newUsername,
+            sharedPreferences.getString(
+                context.getString(R.string.settings_key_profile_username),
+                ""
+            )
+        )
+    }
+
+    @Test
+    fun whenValidEmailChanged_thenValueIsSaved() {
+        val newEmail = "test@example.com"
+        launchFragmentInHiltContainer<ProfileFragment>()
+
+        // Click on email preference
+        onView(withText(R.string.settings_profile_email_title)).perform(click())
+
+        // Type new email and click OK
+        onView(withId(android.R.id.edit)).perform(replaceText(newEmail))
+        onView(withText("OK")).perform(click())
+
+        // Verify that the preference was saved
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        assertEquals(
+            newEmail,
+            sharedPreferences.getString(context.getString(R.string.settings_key_profile_email), "")
+        )
+    }
+
+    @Test
+    fun whenInvalidEmailChanged_thenErrorIsShownAndValueIsNotSaved() {
+        val invalidEmail = "invalid-email"
+        launchFragmentInHiltContainer<ProfileFragment>()
+
+        // Click on email preference
+        onView(withText(R.string.settings_profile_email_title)).perform(click())
+
+        // Type invalid email
+        onView(withId(android.R.id.edit)).perform(replaceText(invalidEmail))
+
+        // Check if error is displayed and OK button is disabled
+        onView(hasErrorText(context.getString(R.string.settings_profile_mail_error)))
+            .check(matches(isDisplayed()))
+        onView(withText("OK")).check(matches(not(isEnabled())))
+
+        // Click cancel
+        onView(withText("Cancel")).perform(click())
+
+        // Verify that the preference was not saved
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        assertEquals(
+            "",
+            sharedPreferences.getString(context.getString(R.string.settings_key_profile_email), "")
+        )
     }
 }
